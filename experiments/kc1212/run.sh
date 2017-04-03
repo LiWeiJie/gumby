2912658eab8913061c004601bf4a76aca0a3dbc6
kc1212 <kc1212@users.noreply.github.com>
2017-04-03 13:53:13 +0200

    Sed substitution

--- experiments/kc1212/run.sh
+++ experiments/kc1212/run.sh
@@ -5,7 +5,5 @@ set -x
 
 cd /var/scratch/pouwelse/jenkins/workspace/pers/consensus_thesis_kcong/consensus-thesis-code
 
-let "n = $DAS4_INSTANCES_TO_RUN"
-let "t = (n-1)/3"
-python -m src.node 4 "$t" -v --discovery "$TRACKER_IP"
+python -m src.node SED_N SED_T -v --discovery SED_TRACKER_IP
 
--- experiments/kc1212/run_discovery.sh
+++ experiments/kc1212/run_discovery.sh
@@ -5,6 +5,11 @@ set -x
 
 cd /var/scratch/pouwelse/jenkins/workspace/pers/consensus_thesis_kcong/consensus-thesis-code
 
-let "m = $DAS4_INSTANCES_TO_RUN"
-python -m src.discovery -n 4 -t 1 -m "$m" -v --inst 10 bootstrap
+# substitute the SED_+ words:
+# sed -i run_discovery.sh 's/SED_N/4'
+# sed -i run_discovery.sh 's/SED_T/1'
+# sed -i run_discovery.sh 's/SED_DELAY/10'
+# sed -i run_discovery.sh 's/SED_EXPERIMENT/bootstrap'
+python -m src.discovery -n SED_N -t SED_T -m "$DAS4_INSTANCES_TO_RUN" -v \
+    --inst SED_DELAY SED_EXPERIMENT
 

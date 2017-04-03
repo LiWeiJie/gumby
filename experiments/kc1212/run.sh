882f11b1f667a5e25c75fb24bcb864d361924724
kc1212 <kc1212@users.noreply.github.com>
2017-04-03 14:07:49 +0200

    Add examples

--- experiments/kc1212/run.sh
+++ experiments/kc1212/run.sh
@@ -5,5 +5,9 @@ set -x
 
 cd /var/scratch/pouwelse/jenkins/workspace/pers/consensus_thesis_kcong/consensus-thesis-code
 
+# substitute the SED_+ words
+# sed -i "s/SED_N/$n/" gumby/experiments/kc1212/run.sh
+# sed -i "s/SED_T/$t/" gumby/experiments/kc1212/run.sh 
+# sed -i "s/SED_TRACKER_IP/$(hostname)/" gumby/experiments/kc1212/run.sh
 python -m src.node SED_N SED_T -v --discovery SED_TRACKER_IP
 
--- experiments/kc1212/run_discovery.sh
+++ experiments/kc1212/run_discovery.sh
@@ -5,11 +5,11 @@ set -x
 
 cd /var/scratch/pouwelse/jenkins/workspace/pers/consensus_thesis_kcong/consensus-thesis-code
 
-# substitute the SED_+ words:
-# sed -i run_discovery.sh 's/SED_N/4'
-# sed -i run_discovery.sh 's/SED_T/1'
-# sed -i run_discovery.sh 's/SED_DELAY/10'
-# sed -i run_discovery.sh 's/SED_EXPERIMENT/bootstrap'
-python -m src.discovery -n SED_N -t SED_T -m "$DAS4_INSTANCES_TO_RUN" -v \
+# substitute the SED_+ words
+# sed -i "s/SED_N/$n/" gumby/experiments/kc1212/run_discovery.sh
+# sed -i "s/SED_T/$t/" gumby/experiments/kc1212/run_discovery.sh
+# sed -i "s/SED_DELAY/$delay/" gumby/experiments/kc1212/run_discovery.sh
+# sed -i "s/SED_EXPERIMENT/bootstrap/" gumby/experiments/kc1212/run_discovery.sh
+python -m src.discovery -n SED_N -t SED_T -m "$DAS4_INSTANCES_TO_RUN" \
     --inst SED_DELAY SED_EXPERIMENT
 

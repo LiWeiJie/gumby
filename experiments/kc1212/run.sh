2e166df3bf1bd255f70817891c070935dda9a5f2
kc1212 <kc1212@users.noreply.github.com>
2017-04-03 14:11:47 +0200

    Start on random port

--- experiments/kc1212/run.sh
+++ experiments/kc1212/run.sh
@@ -7,7 +7,7 @@ cd /var/scratch/pouwelse/jenkins/workspace/pers/consensus_thesis_kcong/consensus
 
 # substitute the SED_+ words
 # sed -i "s/SED_N/$n/" gumby/experiments/kc1212/run.sh
-# sed -i "s/SED_T/$t/" gumby/experiments/kc1212/run.sh 
+# sed -i "s/SED_T/$t/" gumby/experiments/kc1212/run.sh
 # sed -i "s/SED_TRACKER_IP/$(hostname)/" gumby/experiments/kc1212/run.sh
-python -m src.node SED_N SED_T -v --discovery SED_TRACKER_IP
+python -m src.node $(( (RANDOM % 10000) + 40000)) SED_N SED_T -v --discovery SED_TRACKER_IP
 

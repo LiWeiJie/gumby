2f4cc13aef9b442ad6ea27bc5f460a52d8a47369
liweijie <licatweijie@gmail.com>
2018-02-14 11:18:39 +0100

    add say to each

---
+++ experiments/say_to_each/say_to_each_community.py
@@ -0,0 +1,134 @@
+import random
+import struct
+
+from Tribler.dispersy.authentication import MemberAuthentication
+from Tribler.dispersy.community import Community
+from Tribler.dispersy.conversion import BinaryConversion, DefaultConversion
+from Tribler.dispersy.destination import CandidateDestination, CommunityDestination
+from Tribler.dispersy.distribution import FullSyncDistribution, DirectDistribution
+from Tribler.dispersy.message import BatchConfiguration, Message
+from Tribler.dispersy.payload import Payload
+from Tribler.dispersy.resolution import PublicResolution
+
+
+class SayToEachCommunity(Community):
+
+    # @classmethod
+    # def get_master_members(cls, dispersy):
+    #     master_key = "<public-key>".decode("HEX")
+    #     master = dispersy.get
+
+    def __init__(self, dispersy, master_member, my_member):
+        super(SayToEachCommunity, self).__init__(dispersy, master_member, my_member)
+
+        self.modulus = 1000
+        self.n = 5
+        self.guestbook = []
+
+    def initiate_conversions(self):
+        return [DefaultConversion(self), SayToEachCommunityConversion(self)]
+
+    def initiate_meta_messages(self):
+        messages = super(SayToEachCommunity, self).initiate_meta_messages()
+
+        ourmessages = [Message(self,
+                               u"say-to-others",
+                               MemberAuthentication(),
+                               PublicResolution(),
+                               FullSyncDistribution(u"ASC", 128, False),
+                               CommunityDestination(10),
+                               SayToOthersPayload(),
+                               self._generic_timeline_check,
+                               self.on_say,
+                               batch=BatchConfiguration(0.0)),
+                       Message(self,
+                               u"say-to-locals",
+                               MemberAuthentication(),
+                               PublicResolution(),
+                               DirectDistribution(),
+                               CandidateDestination(),
+                               SayToOthersPayload(),
+                               self._generic_timeline_check,
+                               self.on_say,
+                               batch=BatchConfiguration(0.0))
+                       ]
+        messages.extend(ourmessages)
+
+        return messages
+
+    def on_say(self, messages):
+        for message in messages:
+            self.guestbook.append( message.payload.who + " say : " + message.payload.text )
+
+    def say_to_others(self):
+        meta = self.get_meta_message(u"say-to-others")
+        message = meta.impl(authentication=(self._my_member,),
+                            distribution=(self.claim_global_time(),),
+                            payload=(self.get_master_members().__str__(), "hello"))
+        self.dispersy.store_update_forward([message], False, False, True)
+
+    def say_to_locals(self):
+        meta = self.get_meta_message(u"say-to-locals")
+        messages = []
+        for candidate in self.dispersy_yield_verified_candidates():
+            messages.append(meta.impl(authentication=(self._my_member,),
+                                      distribution=(self.claim_global_time(),),
+                                      destination=(candidate,),
+                                      payload=(self.get_master_members().__str__(), "hello")))
+        self.dispersy.store_update_forward(messages, False, False, True)
+
+class SayToOthersPayload(Payload):
+    class Implementation(Payload.Implementation):
+        def __init__(self, meta, who, text):
+            assert isinstance(who, str)
+            assert isinstance(text, str)            
+            super(SayToOthersPayload.Implementation, self).__init__(meta)
+            self._who = who
+            self._text = text
+
+        @property
+        def who(self):
+            return self._who
+        
+        @property
+        def text(self):
+            return self._text
+ 
+
+class SayToEachCommunityConversion(BinaryConversion):
+
+    def __init__(self, community):
+        super(SayToEachCommunityConversion, self).__init__(community, "\x01")
+        self.define_meta_message(
+            chr(1),
+            community.get_meta_message(u"say-to-others"),
+            self._encode_say,
+            self._decode_say)
+        self.define_meta_message(
+            chr(2),
+            community.get_meta_message(u"say-to-locals"),
+            self._encode_say,
+            self._decode_say)
+
+    def _encode_say(self, message):
+        return encode((message.payload.who, message.payload.text)),
+
+    def _decode_share(self, placeholder, offset, data):
+        try:
+            offset, payload = decode(data, offset)
+        except ValueError:
+            raise DropPacket("Unable to decode the say-payload")
+
+        if not isinstance(payload, tuple):
+            raise DropPacket("Invalid payload type")
+
+        who, text = payload
+        if not isinstance(text, str):
+            raise DropPacket("Invalid 'text' type")
+        if not isinstance(who, str):
+            raise DropPacket("Invalid 'who' type")
+
+        return offset, placeholder.meta.payload.implement(who, text)
+        # value, = self.share_format.unpack_from(data, offset)
+        # offset += self.share_format.size
+        # return offset, placeholder.meta.payload.implement(value)
---
+++ experiments/say_to_each/say_to_each_module.py
@@ -0,0 +1,76 @@
+from gumby.experiment import experiment_callback
+from gumby.modules.community_experiment_module import CommunityExperimentModule
+from gumby.modules.community_launcher import CommunityLauncher
+from gumby.modules.experiment_module import static_module
+from gumby.modules.isolated_community_loader import IsolatedCommunityLoader
+
+from .say_to_each_community import SayToEachCommunity
+
+
+class SayToEachCommunityLoader(IsolatedCommunityLoader):
+    """
+    This provides the capability to run your communities in an isolated fashion.
+    You can include multiple launchers here.
+    """
+
+    def __init__(self, session_id):
+        super(SayToEachCommunityLoader, self).__init__(session_id)
+        self.set_launcher(SayToEachCommunityLauncher())
+
+
+class SayToEachCommunityLauncher(CommunityLauncher):
+    """
+    This class forwards all the information Dispersy needs to launch our community.
+    """
+    def get_community_class(self):
+        return SayToEachCommunity
+
+    def get_my_member(self, dispersy, session):
+        return dispersy.get_new_member()
+
+    def get_kwargs(self, session):
+        return {}
+
+    def get_name(self):
+        return "SayToEachCommunity2Weijie"
+
+
+
+@static_module
+class SayToEachModule(CommunityExperimentModule):
+    """
+    This is the module we reference through the scenario (note @static_module).
+    All of the functionality we want to expose to the scenario is marked `@experiment_callback`.
+    """
+    def __init__(self, experiment):
+        super(SayToEachModule, self).__init__(experiment, SayToEachCommunity)
+        self.dispersy_provider.custom_community_loader = SayToEachCommunityLoader(self.dispersy_provider.session_id)
+
+    @experiment_callback
+    def say_to_others(self):
+        self.community.say_to_others()
+
+    @experiment_callback
+    def say_to_locals(self):
+        self.community.say_to_locals()
+
+    @experiment_callback
+    def print_guestbook(self):
+        print "guest book size: %d\n"%self.community.guestbook.__len__()
+        print self.community.guestbook
+
+    # @experiment_callback
+    # def share_local(self):
+    #     self.community.share_local()
+
+    # @experiment_callback
+    # def share_subset_sum(self):
+    #     self.community.share_subset_sum()
+
+    # @experiment_callback
+    # def print_local_value(self):
+    #     print "My secret value is", self.community.my_secret_share
+
+    # @experiment_callback
+    # def print_total_sum(self):
+    #     print "My computed sum is", self.community.total_sum

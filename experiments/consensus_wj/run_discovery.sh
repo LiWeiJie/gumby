ad4a639048949f81cd09b691ab67ac3f771fb996
liweijie <licatweijie@gmail.com>
2018-03-12 11:19:45 +0100

    .

--- experiments/consensus_wj/run_discovery.sh
+++ experiments/consensus_wj/run_discovery.sh
@@ -3,6 +3,7 @@
 set -e
 set -x
 
+pwd
 cd /var/scratch/pouwelse/jenkins/workspace/pers/test_consensus_weijie/consensus-code
 
 # e.g. python -m src.discovery -n SED_N -t SED_T -m "$DAS4_INSTANCES_TO_RUN" --inst SED_DELAY SED_EXPERIMENT SED_PARAM

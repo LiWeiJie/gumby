ded9bb14f07d0280139c0866d2025687ea15b9e1
liweijie <licatweijie@gmail.com>
2018-03-12 12:35:28 +0100

    ....

--- experiments/consensus_wj/run_discovery.sh
+++ experiments/consensus_wj/run_discovery.sh
@@ -1,9 +1,1 @@
 #!/usr/bin/env bash
-
-set -e
-set -x
-
-cd /var/scratch/pouwelse/jenkins/workspace/pers/consensus_thesis_kcong/consensus-thesis-code
-
-# e.g. python -m src.discovery -n SED_N -t SED_T -m "$DAS4_INSTANCES_TO_RUN" --inst SED_DELAY SED_EXPERIMENT SED_PARAM
-DISCOVERY_COMMAND
